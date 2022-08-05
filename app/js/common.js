5023431e158e410fc334d44a55b169de4b69adce
Viktoria <zaharova181114@gmail.com>
2022-08-05 21:45:27 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,6 @@
+$('.hits-slider').slick({
+    slidesToShow: 6,
+    appendArrows: '.hits-slider__nav',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+});
